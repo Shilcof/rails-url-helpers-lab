1595613e4f5cc691c9c14a5d062cda4685199470
Shilcof <robert.shilcof@gmail.com>
2020-12-31 19:38:13 +0000

    commit tree 95859035895c07c20559d430b93d58f3e50ce7ab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:show, :index]
+
+  get "activate/:id", to: "students#activate", as: "activate_student"
+
+  # get 'about', to: 'static#about'
+
 end
